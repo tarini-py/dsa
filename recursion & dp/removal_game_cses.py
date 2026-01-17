aa91f19b0b660fbb3d1cd6b0fab37cfba80df357
Tarini Prasad Samantray <tp.samantray@gmail.com>
2026-01-17 17:19:59 +0530

    Refactor removal game solution with memoization
    Rewrote the recursive function to use memoization for efficiency and corrected the game logic to use the minimax approach. Added input handling and increased recursion limit to support larger inputs.

--- recursion & dp/removal_game_cses.py
+++ recursion & dp/removal_game_cses.py
@@ -1,31 +1,37 @@
+import sys
+sys.setrecursionlimit(3000)
 
-def f(n, left, right):
+def f(left, right, dp):
     
-    if n <= 0:
+    if left > right:
         return 0
     
-    score = 0
+    if (left, right) in dp:
+        return dp[(left, right)]
 
-    if left < right and left < n and right > 0:
-        score = max(
-                arr[left] + max(f(n-2, left+2, right), f(n-2, left+1, right-1)),
-                arr[right] + max(f(n-2, left+1, right-1), f(n-2, left, right-2))
-        )
+    score = max(
+            arr[left] + min( f(left+2, right, dp), f(left+1, right-1, dp)),
+            arr[right] + min(f(left+1, right-1, dp), f(left, right-2, dp))
+    )  
 
-    return score
+    dp[(left, right)] = score
+    return dp[(left, right)]
+
+
+arr_len = int(input())
+arr = [int(i) for i in input().split()]
+# n = 4
+# arr = [4,5,1,3]
+
+l = 0
+r = arr_len - 1
+dp = {}
+if arr_len - 1 == 0:
+    print(arr[0])
+else:
+    print(f(l, r, dp))
     
-    
-
-
-
-
-n = 4
-arr = [4,5,1,3]
-
-left = 0
-right = len(arr) - 1
-
-print(f(n, left, right))
+print(dp)
     
     
     
